97c3ca8af7ff04f251b975d6d6d3cfaf605fe1e6
Matthew <xuwanjin1991@Gmail.com>
2020-08-09 18:56:27 +0800

    use package
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

--- flutter_learning/lib/main.dart
+++ flutter_learning/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,14 +8,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center(
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
