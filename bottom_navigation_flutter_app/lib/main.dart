f80a5b7dc63ad389db1dcdce999f8103a10d9486
Matthew <xuwanjin1991@Gmail.com>
2020-08-17 11:26:12 +0800

    4 items for BottomNavigation of Flutter SDK
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

--- bottom_navigation_flutter_app/lib/main.dart
+++ bottom_navigation_flutter_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "Matthew Bottom Navigation",
-    home: BottomNavigationBar3Items(),
+    home: BottomNavigationBar4Items(),
   ));
 }
 
@@ -42,4 +42,53 @@ class BottomNavigationBar3ItemsState extends State<BottomNavigationBar3Items> {
   }
 }
 
+class BottomNavigationBar4Items extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return BottomNavigationBar4ItemsState();
+  }
+}
+
+class BottomNavigationBar4ItemsState extends State<BottomNavigationBar4Items> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.red, // 给 每一个item 背景颜色, 这样就不会出现 item 图标的颜色和 背景色一样的情况
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Favorite",
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'List',
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+              backgroundColor: Colors.red,
+            ),
+          ],
+          currentIndex: selectedIndex,
+          // 必须给 currentIndex 赋 当前选中的 index（就是 item的 index） item才能点击便色
+          onTap: onItemTaped,
+        ));
+  }
+
+  void onItemTaped(int index) {
+    setState(() {
+      selectedIndex = index; // selectedIndex 赋予当前点击的 item 的 index
+    });
+  }
+}
 
